7cfcf2b9365a111a4e2ce3062ad5097aa73575b0
Mayur More <m.mayur404@gmail.com>
2018-07-31 20:28:36 +0530

    xa

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,6 @@
-
 var log = true;
 log && console.log("Welcome to Letter");
-var content = "Hello World!! How do you do??";
+log && console.log(window.location.hash);
 
 var DATA_PREFIX = "data:text/html;base64,";
 var DATA_PREFIX_8 = "data:text/html;charset=utf-8;base64,";
@@ -10,11 +9,38 @@ var DATA_PREFIX_BXZE = "data:text/html;charset=utf-8;bxze64,";
 var BASE64_MARKER = ';base64,';
 var LZMA64_MARKER = ';bxze64,';
 
+var state;
+var title;
+var href = window.location.href; 
+var data;
+var zip;
+var hash = window.location.hash;
+var content;
+var contentUpdated = false;
+var interval;
+
+if(hash){
+    data = href.slice(href.indexOf('#') + 1).split('?');    
+    if(data[0] == 'create'){
+        state = 'new'
+    }else{
+        state = 'view';
+    }
+    
+}else{
+    data = ['',''];
+    state = 'new';
+    //updateLink('Untitled','XQAAAAIqAAAAAAAAAAAkGUmYbxARyF/m1YpoE5LmewkUWEGHN2/DCn6Pb9Bq6N3zZjOPRDwKDY/oQBj//eEQAA==');
+}
+
+
 var getUrlVars = () => {
     var vars = [], hash;
     var hashes = window.location.href.slice(window.location.href.indexOf('#') + 1).split('&');
     return hashes;
 }
+var titleDOM = document.getElementById("title");
+var contentDOM = document.getElementById("content");
 
 
 var domReady = function () {
@@ -38,14 +64,79 @@ var domReady = function () {
         mdc.ripple.MDCRipple.attachTo(el);
     });
     
-    $('.progress').hide();
+    var menuEl = document.querySelector('#demo-menu');
+    var menu = new mdc.menu.MDCMenu(menuEl);
+    var menuButtonEl = document.querySelector('#menu-button');
+    menuButtonEl.addEventListener('click', function () {
+        menu.open = !menu.open;
+    });
+
+
+    var anchor = document.querySelector('.mdc-menu-anchor');
+    // Initialize to top left.
+    anchor.style.setProperty('top', '0');
+    anchor.style.setProperty('right', '0');
+
     
-    stringToZip(content,(result) =>{
+    
+    $('.progress').hide();
+    log && console.log(state);
+    log && console.log(data);
+    
+    $(document).on('change keydown keypress input', 'div[data-placeholder]', function () {
+        
+        if (this.textContent) {
+            this.dataset.divPlaceholderContent = 'true';
+        }
+        else {
+            delete (this.dataset.divPlaceholderContent);
+        }
+    });
+
+
+    contentDOM.addEventListener("keyup", handleInput);
+    titleDOM.addEventListener("keyup", handleInput);
+    
+
+
+    switch(state){
+        case 'new':
+            updateLink(null,null);
+        break;
+        case 'view':
+            initDocument(data,(content)=>{
+                log && console.log("Init Complete");
+                log && console.log(content);
+            });
+        break;
+    }
+    
+    /*stringToZip(content,(result) =>{
         log && console.log(result);
         zipToString(result,(result)=>{
             log && console.log(result);
         })
-    });
+    });*/
+
+    interval = setInterval(()=>{
+        if(contentUpdated){
+            log && console.log('Content Updated This instance');
+            contentUpdated = false;
+            log && console.log(content);
+            if(!title && !content){
+                log && console.log("Came Here tooo");
+                updateLink(null, null);
+            }else if(content) {
+                stringToZip(content, (result) => {
+                    zip = result;
+                    log && console.log(zip);
+                    updateLink(title, zip);
+                });
+            }
+        }else{
+            log && console.log("Unaltered");
+        }
+    },1000);
 
 
 };
@@ -59,3 +150,64 @@ if (document.readyState === "complete" || (document.readyState !== "loading" && 
 var compressContent = (content) =>{
     log && console.log("Compressing now");
 }
+
+var initDocument = (data,callback) =>{
+    log && console.log("Inititalising Document");
+    title = data[0];
+    zipToString(data[1], (result) => {
+        callback(result);
+    })
+}
+
+function updateLink(title, zip, push) {
+    log && console.log("Updating Link");
+    log && console.log(title);
+    log && console.log(zip);
+    if (title) title = encodeURIComponent(title.trim().replace(/\s/g, "_"));
+    var url;
+    if (zip && zip.length) {
+        url = "/#" + (title || "untitled") + "?" + zip;
+    } else if(title && !(zip && zip.length)){
+        url = "/#" + (title || "untitled") + "?";
+    }else if(!title && !(zip && zip.length)){
+        url = "/#create";
+    }
+    log && console.log(url);
+    var hash = location.hash;
+    if (!hash || !hash.length) {
+        log && console.log(hash);
+        log && console.log("If State");
+        window.history.pushState({"content":content}, null, url);
+    } else {
+        log && console.log(hash);
+        log && console.log("Else State");
+        window.history.replaceState({"content":content}, null, url);
+    }
+    var length = location.href.length;
+    log && console.log(`Total size of the document: ${length} bytes`);
+    /*QS("#length").innerText = length + " bytes";
+    QS("#length").href = url;
+    for (var key in maxLengths) {
+        var maxLength = maxLengths[key];
+        if (length > maxLength) {
+            QS(key).classList.add("invalid");
+        } else {
+            QS(key).classList.remove("invalid");
+        }
+    }*/
+}
+
+
+
+function handleInput(e,log = false) {
+    log && console.log("Handling Input for Either and Both Title and Content");
+    content = contentDOM.innerText;
+    title = titleDOM.innerText;
+    log && console.log(content);
+    log && console.log(title);
+    contentUpdated = true;
+    /*
+    */
+}
+
+
